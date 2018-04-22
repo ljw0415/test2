658f9977a480c41dc1151409b191fca2afde21c2
ljwcry <512548376@qq.com>
2018-04-22 21:17:32 +0800

    zhbj74

---
+++ zhbj74/src/com/itheima/zhbj74/fragment/ContentFragment1.java
@@ -0,0 +1,19 @@
+package com.itheima.zhbj74.fragment;
+
+import android.view.View;
+
+public class ContentFragment1 extends BaseFragment {
+
+	@Override
+	public View initView() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void initData() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
